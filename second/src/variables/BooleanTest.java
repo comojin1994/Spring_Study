581f932ebe5940cdb3d0c18c52666aace3fd8eff
comojin1994 <comojin1994@gmail.com>
2020-07-01 00:05:02 +0900

    Add second java project

---
+++ second/src/variables/BooleanTest.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        boolean isMarried = false;
+        System.out.println(isMarried);
+
+        var i = 10;
+        System.out.println(i);
+    }
+}
---
+++ second/src/variables/DoubleTest.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class DoubleTest {
+    public static void main(String[] args) {
+        double dNum = 3.14;
+        float fNum = 3.14f;
+        System.out.println(dNum);
+        System.out.println(fNum);
+    }
+}
