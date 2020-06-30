33e0d693f1008aeb0a1ab97b410e04fa0e444322
comojin1994 <comojin1994@gmail.com>
2020-07-01 00:05:17 +0900

    Add third java project

--- Chapter3/src/operator/OperatorEx3.java
+++ Chapter3/src/operator/OperatorEx3.java
@@ -1,4 +1,33 @@
 package operator;
 
 public class OperatorEx3 {
+    public static void main(String[] args) {
+        int score = 100;
+        System.out.println(++score);
+        System.out.println(score++);
+        System.out.println(score);
+        System.out.println(--score);
+        System.out.println(score--);
+
+        int num1 = 10;
+        int i = 2;
+
+        boolean value = ((num1 = num1 + 10) < 10) && ((i = i + 2) < 10);
+        System.out.println(num1);
+        System.out.println(i);
+
+        System.out.println(value);
+
+        value = ((num1 = num1 + 10) < 10) || ((i = i + 2) < 10);
+        System.out.println(num1);
+        System.out.println(i);
+
+        System.out.println(value);
+
+        int num2 = 10;
+        int num3 = 20;
+
+        int num = (num2 > num3)? num2: num3;
+        System.out.println(num);
+    }
 }
