dd85b242af17e513108b20d62051cc09c752f64f
comojin1994 <comojin1994@gmail.com>
2020-07-23 20:56:42 +0900

    Add eatgo project

---
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -0,0 +1,25 @@
+package kr.co.fastcampus.eatgo.domain;
+
+public class Restaurant {
+
+    private final String name;
+
+    private final String address;
+
+    public Restaurant(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getInformation() {
+        return this.name + " in " + this.address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
---
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -0,0 +1,10 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestaurantController {
+
+
+
+}
